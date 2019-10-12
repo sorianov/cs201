d8414844352c7482df9aa047ca4be1eafb16b699
Victor Soriano Mendoza <soriano.victorm@student.clackamas.edu>
2019-10-11 20:00:57 -0700

    add first assignment

---
+++ asgn1/hello.c
@@ -0,0 +1,12 @@
+/*********************************************
+* Program: Asgn0 - part 2
+* Author: Victor Soriano Mendoza
+* Date:   10/2/2019
+**********************************************/
+#include <stdio.h>
+
+int main() {
+    printf("I love vim.\n");
+    printf("Victor Soriano Mendoza\n\n");
+    return 1;
+}

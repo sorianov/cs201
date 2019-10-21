470290c3eb6339d0f43a743d3778ff59cbb26c2a
Victor Soriano Mendoza <soriano.victorm@student.clackamas.edu>
2019-10-20 17:25:53 -0700

    finish part one of assignment 3

--- asgn3/floatfun.c
+++ asgn3/floatfun.c
@@ -2,6 +2,23 @@
 #include <stdlib.h>
 
 const int BITS_PER_BYTE = 8;
+const int EXP_START_BIT = 30;
+const int EXP_END_BIT = 23;
+const int FRAC_START_BIT = 22;
+const int FRAC_END_BIT = 0;
+
+union floatInt {
+    float f;
+    int i;
+};
+
+/** Function declarations **/
+void printFractionalBits(int num);
+void printExpBits(int num);
+void printSignBit(int num);
+void printAsBinary(int num, int numBits);
+void printAuthor();
+/**************************/
 
 /**
  * Prints the name of the author of this code.
@@ -34,15 +51,14 @@ int nthBit(int n, int k) {
  * Prints a short in a n-byte binary representation.
  *
  * Input:
- * num - value to print
- * numBytes - how many bytes to print
+ * int num - value to print
+ * int numBits - how many bits to print
  *
  * Output:
  * Prints binary representation with two spaces between bytes
  * and one space between nibbles.
  */
-void printAsBinary(int num, int numBytes) {
-    int numBits = BITS_PER_BYTE * numBytes;
+void printAsBinary(int num, int numBits) {
     int modEight = 0;
     int modFour = 0;
 
@@ -59,38 +75,113 @@ void printAsBinary(int num, int numBytes) {
     }
 }
 
+/**
+ * Prints information about the sign bits of a float. This function assumes
+ * a union between a float and an int is being used. It also assumes a single
+ * precision float.
+ *
+ * Input:
+ * num - the int representation in the union
+ *
+ * Output:
+ * The decimal, hexadecimal, and binary representation of bit 31.
+ */
 void printSignBit(int num) {
     int signBit = nthBit(num, 31);
-    printf("Sign:     %d\t%x\t", signBit, signBit);
-    printAsBinary(signBit, 1);
+    printf("Sign:          %d       0x%x   %d", 
+        signBit, 
+        signBit, 
+        signBit);
     puts("");
 }
 
+/**
+ * A fake pow() that has only been slightly tested using 2 as a
+ * base. Not guaranteed to work on negative exponents. Created because the
+ * compilation flags would need to be altered to link against <math.h>.
+ *
+ * Input:
+ * int base - the base to use
+ * int exponent - exponent to raise the base to
+ *
+ * Return:
+ * int value - the base multiplied by itself exponent times.
+ */
+int _pow(int base, int exponent) {
+    int value = 1;
+    for (int i = 0; i < exponent; ++i) {
+       value = value * base;
+    }
+    return value;
+}
+
+/**
+ * Prints information about the exponent bits of a float. Assumes a union is
+ * being used with a float and int.
+ *
+ * Input:
+ * int num - the integer part of the union.
+ *
+ * Output:
+ * Prints the decimal, hexadecimal, and binary representation of bits 30-23
+ */
+void printExpBits(int num) {
+    int dec = 0;
+    int bit = (EXP_START_BIT - EXP_END_BIT) - 1;
+    for (int i = EXP_START_BIT; i >= EXP_END_BIT; --i) {
+        dec = dec + (_pow(2, bit) * nthBit(num, i));
+        --bit;
+    }
+    printf("Exp bits:    %d      %#x   ", dec, dec);
+    printAsBinary(dec, 8);
+}
+
+/**
+ * Prints information about the fractional bits of a float. Assumes a union
+ * is being used with a float and int.
+ *
+ * Input:
+ * int num - the integer part of the union.
+ *
+ * Output:
+ * Prints the decimal, hexadecimal, and binary representation of bits 22-0
+ */
+void printFractionalBits(int num) {
+    int dec = 0;
+    int bit = (FRAC_START_BIT - FRAC_END_BIT) - 1;
+    for (int i = FRAC_START_BIT; i >= FRAC_END_BIT; --i) {
+        dec = dec + (_pow(2, bit) * nthBit(num, i));
+        --bit;
+    }
+    if (dec == 0) {
+        printf("Fraction:    %d     0x%#05x   ", dec, dec);
+    } else {
+        printf("Fraction:    %d     %#05x   ", dec, dec);
+    }
+    printAsBinary(dec, 19);
+
+}
+
 int main(int argc, char* argv[]) {
-    float y;
 
     // Verify we recieved the correct amount of arguments
-    if (argc != 2) {
-        printf("Usage: %s float_value\n", argv[0]);
+    if (argc != 2) { printf("Usage: %s float_value\n", argv[0]);
         return -1;
     }
 
-    // Convert c-string argument into float
-    y = atof(argv[1]);
+    union floatInt fInt = { .f = atof(argv[1]) };
 
-    union {
-        float f;
-        int i;
-    } fInt = { .f = y };
-
-    printAuthor();
-    puts("");
-    printf("Float val:   %f\n", y);
+    printAuthor(); puts(""); printf("Float val:   %f\n", fInt.f);
     printf("In hex:      %#0x\n", fInt.i);
     printf("In binary:   ");
     printAsBinary(fInt.i, 4);
-    puts("");
+    puts("\n");
     printSignBit(fInt.i);
+    printExpBits(fInt.i);
+    puts("\n");
+    printFractionalBits(fInt.i);
+    puts("\n");
+
 
     return 0;
 }

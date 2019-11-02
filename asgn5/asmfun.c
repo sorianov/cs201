e23ca68338c80737966ac151882719f7725df5f9
Victor Soriano Mendoza <vsoriano@hotp.com>
2019-11-01 17:40:03 -0700

    add working implementation of printCpuVendorID()

--- asgn5/asmfun.c
+++ asgn5/asmfun.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+const int INT_REGISTER_LENGTH = 4; // bytes
+
 double sphereVolume(const double radius) {
     double volume;
     const double four = 4.0;
@@ -32,10 +34,48 @@ double sphereVolume(const double radius) {
     return volume;
 }
 
+void printBytesAscii(int r, int numBytes) {
+    char* c;
+
+    c = (char*) &r;
+    for (size_t i = 0; i < numBytes; ++i) {
+        printf("%c", *c);
+        ++c;
+    }
+}
+
+void printCpuVendorID() {
+    int b; // ebx
+    int c; // ecx
+    int d; // edx
+
+    asm(
+            "   mov     $0,     %%eax   \n"     // Make %eax 0 so we get
+                                                // vendor id in registers
+                                                // %ebx, %edx, and %ecx
+            "   cpuid                   \n"
+            "   movl    %%ebx,  %[b]    \n"     // first part of vendor id
+            "   movl    %%edx,  %[d]    \n"     // second part of vendor id
+            "   movl    %%ecx,  %[c]    \n"     // third part of vendor id
+
+            :   [b]         "=m"        (b),    // outputs
+                [c]         "=m"        (c),
+                [d]         "=m"        (d)
+            :                                   // inputs
+
+            :   "eax", "ebx", "ecx", "edx"      // clobbers
+        );
+
+    printBytesAscii(b, INT_REGISTER_LENGTH);
+    printBytesAscii(d, INT_REGISTER_LENGTH);
+    printBytesAscii(c, INT_REGISTER_LENGTH);
+    puts("");
+}
 
 int main(void) {
     double r = 4.5;
     printf("Volume of sphere with radius %.3f: %.3f\n", r, sphereVolume(r));
+    printCpuVendorID();
 
     return 0;
 }

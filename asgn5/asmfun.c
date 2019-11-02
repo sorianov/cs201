29d59789a9af3589f3fd1715d32e3981ddff63d6
Victor Soriano Mendoza <vsoriano@hotp.com>
2019-11-01 17:36:08 -0700

    add working implementation of sphereVolume

--- asgn5/asmfun.c
+++ asgn5/asmfun.c
@@ -1,20 +1,41 @@
-double sphereVolume(double radius) {
+#include <stdio.h>
+
+double sphereVolume(const double radius) {
     double volume;
+    const double four = 4.0;
+    const double three = 3.0;
 
     asm(
-        "fldl      $3.0             \n"
-        "fldl      $4.0             \n"
-        "fdivp                    \n"
-        "fstpl     %[volume]      \n"
-        :   [volume]    "=m"(volume)    // outputs
+        "   fldl        %[three]        \n" // load 3.0
+        "   fldl        %[four]         \n" // load 4.0
+        "   fdivp                       \n" // 4/3
+        "   fldpi                       \n" // load PI constant
+        "   fmulp                       \n" // (4/3) * PI
+        "   fldl        %[radius]       \n" // load radius argument
+        "   fldl        %[radius]       \n"
+        "   fldl        %[radius]       \n"
+        "   fmulp                       \n" // radius^2
+        "   fmulp                       \n" // radius^3
+        "   fmulp                       \n" // ((4/3) * PI) * radius^3
+        "   fstpl       %[volume]       \n" // store into volume
+
+        :   [volume]    "=m"    (volume)    // output
+
+        :   [four]      "m"     (four),     // inputs
+            [three]     "m"     (three),
+            [radius]    "m"     (radius)
+
+        :   "st"                            // clobbers. Just in case this
+                                            // was already in use.
     );
 
     return volume;
 }
 
+
 int main(void) {
-    double r = 1;
-    printf("Volume of sphere with radius %f: %f\n", r, sphereVolume(r));
+    double r = 4.5;
+    printf("Volume of sphere with radius %.3f: %.3f\n", r, sphereVolume(r));
 
     return 0;
 }
